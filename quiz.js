f6d4064403d2862c32325028102fdcf83c1a20b0
Jennifer Sha <jenniferlaurasha@gmail.com>
2016-07-13 00:35:41 -0700

    Initial commit

---
+++ quiz.js
@@ -0,0 +1,104 @@
+/**
+ * Created by jennifersha on 7/4/16.
+ */
+
+window.onload = function(){
+    typedName.focus();
+};
+
+
+
+
+var allQuestions = document.querySelectorAll(".question");
+
+
+
+function hideAllQuestions (){
+    for (var i=0; i< allQuestions.length; i++){
+        allQuestions[i].style.display="none";
+    }
+}
+
+hideAllQuestions();
+
+
+
+
+var typedName = document.getElementById("entername").getElementsByTagName("input")[0];
+
+var submitName = document.getElementById("entername").getElementsByTagName("input")[1];
+
+
+submitName.addEventListener("click", function(){
+    if(typedName.value.length>0){
+        document.getElementById("entername").style.display="none";
+        allQuestions[0].style.display = "block";
+        allQuestions[0].getElementsByTagName("button")[0].style.display = "none";
+
+    }
+    else {
+        alert("Please enter your name so we can get started!");
+        typedName.focus();
+    }
+}, false);
+
+
+
+
+typedName.addEventListener("keyup", function(event){
+    if(event.which == 13){
+        event.preventDefault();
+        submitName.click();
+    } else {}
+}, false);
+
+
+
+var nextButtons = document.getElementsByClassName("button");
+for (var i = 0; i < nextButtons.length; i++) {
+    var currentButton = nextButtons[i];
+    currentButton.addEventListener('click', function(event) {
+        var parent = event.target.parentElement;
+        parent.style.display = "none";
+        var nextQuestion = parent.nextSibling.nextSibling;
+        nextQuestion.style.display = "block";
+    }, false)
+}
+
+
+for(var x=0; x<allQuestions.length; x++){
+    if(x!==2){
+        var questionChoices = allQuestions[x].getElementsByTagName("input");
+        for (var i = 0; i<questionChoices.length; i++){
+            currentQuestionChoice = questionChoices[i];
+            currentQuestionChoice.addEventListener("click",function(event) {
+                var buttonBelow = event.target.parentElement.parentElement.lastElementChild;
+                buttonBelow.style.display = "inline";
+                var nextQuestionButton = event.target.parentElement.parentElement.nextSibling.nextSibling.lastElementChild;
+                nextQuestionButton.style.display = "none";
+            },false)
+        }
+    }else{
+        var questionThreeChoices = allQuestions[2].getElementsByTagName("input");
+        for (var k=0; k<questionThreeChoices.length; k++) {
+            var currentQuestionThreeChoice = questionThreeChoices[k];
+            var y = 0;
+            currentQuestionThreeChoice.addEventListener("click", function (event) {
+                if (event.target.checked) {
+                    y++
+                } else {
+                    y--
+                }
+                ;
+                var buttonBelow = event.target.parentElement.parentElement.lastElementChild;
+                if (y == 2) {
+                    buttonBelow.style.display = "inline";
+                    var nextQuestionButton = event.target.parentElement.parentElement.nextSibling.nextSibling.lastElementChild;
+                    nextQuestionButton.style.display = "none";
+                } else {
+                    buttonBelow.style.display = "none";
+                }
+            }, false)
+        }
+    }}
+

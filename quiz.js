53c6c974d4d0e59272f6f582a04273e1f2c0af0e
Jennifer Sha <jenniferlaurasha@gmail.com>
2016-07-17 17:42:06 -0700

    Refactor code.

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@
  * Created by jennifersha on 7/4/16.
  */
 
-window.onload = function(){
+window.onload = function () {
     typedName.focus();
 };
 
@@ -10,13 +10,10 @@ window.onload = function(){
 
 
 var allQuestions = document.getElementsByClassName("question");
-
-console.log(allQuestions);
-
-
 function hideAllQuestions (){
-    for (var i=0; i< allQuestions.length; i++){
-        allQuestions[i].style.display="none";
+    for (var i = 0; i < allQuestions.length; i++) {
+        var currentQuestion = allQuestions[i];
+        currentQuestion.style.display = "none";
     }
 }
 
@@ -24,32 +21,26 @@ hideAllQuestions();
 
 
 
-
+var topBanner = document.getElementById("welcomeScore");
 var typedName = document.getElementById("entername").getElementsByTagName("input")[0];
-
 var submitName = document.getElementById("entername").getElementsByTagName("input")[1];
 
-function welcomeUser () {
-    var userNameEntered = document.getElementById("entername").getElementsByTagName("input")[0].value;
-    document.getElementById("welcomeScore").innerHTML="Welcome, " + userNameEntered + "!";
+function welcomeUser() {
+    var userNameEntered = typedName.value;
+    topBanner.innerHTML = "Welcome, " + userNameEntered + "!";
 }
 
-submitName.addEventListener("click", function(){
-    if(typedName.value.length>0){
-        document.getElementById("entername").style.display="none";
+submitName.addEventListener("click", function () {
+    if (typedName.value.length > 0) {
+        document.getElementById("entername").style.display = "none";
         allQuestions[0].style.display = "block";
         allQuestions[0].getElementsByTagName("button")[0].style.display = "none";
         welcomeUser();
-
-    }
-    else {
+    } else {
         alert("Please enter your name so we can get started!");
         typedName.focus();
     }
 }, false);
-
-
-
 
 typedName.addEventListener("keyup", function(event){
     if(event.which == 13){
@@ -59,53 +50,37 @@ typedName.addEventListener("keyup", function(event){
 
 
 
+
 var nextButtons = document.getElementsByClassName("button");
+
 for (var z = 0; z < nextButtons.length; z++) {
     var currentButton = nextButtons[z];
-    currentButton.addEventListener('click', function(event) {
+    currentButton.addEventListener('click', function (event) {
         var parent = event.target.parentElement;
         parent.style.display = "none";
         var nextQuestion = parent.nextSibling.nextSibling;
         nextQuestion.style.display = "block";
-        
-        
-
-        /**var numberCorrect = 0;
-         var userScore = document.getElementById("welcomeScore").innerHTML = "Your score: " + numberCorrect;
-         var currentQuestionChoices = event.target.previousSibling.getElementsByTagName("input");
-         for (var w = 0; w < currentQuestionChoices.length; w++){
-            if (currenQuestionChoices[w].checked == 0) {
-                alert("hi");
-            }else{userScore()}}}**/
-
     }, false)
-    
 }
 
 
 
-var answeredCorrect = 0;
 
-
-
-
-for(var x=0; x<allQuestions.length; x++){
-    if(x!==2){
+for (var x = 0; x < allQuestions.length; x++) {
+    if (x !== 2){
         var questionChoices = allQuestions[x].getElementsByTagName("input");
-        for (var i = 0; i<questionChoices.length; i++){
+        for (var i = 0; i < questionChoices.length; i++){
             var currentQuestionChoice = questionChoices[i];
-            currentQuestionChoice.addEventListener("click",function(event) {
+            currentQuestionChoice.addEventListener("click" ,function (event) {
                 var buttonBelow = event.target.parentElement.parentElement.lastElementChild;
                 buttonBelow.style.display = "inline";
                 var nextQuestionButton = event.target.parentElement.parentElement.nextSibling.nextSibling.lastElementChild;
                 nextQuestionButton.style.display = "none";
-                
-
-            },false)
+            }, false)
         }
-    }else{
+    } else {
         var questionThreeChoices = allQuestions[2].getElementsByTagName("input");
-        for (var k=0; k<questionThreeChoices.length; k++) {
+        for (var k = 0; k < questionThreeChoices.length; k++) {
             var currentQuestionThreeChoice = questionThreeChoices[k];
             var y = 0;
             currentQuestionThreeChoice.addEventListener("click", function (event) {
@@ -113,7 +88,7 @@ for(var x=0; x<allQuestions.length; x++){
                     y++
                 } else {
                     y--
-                };
+                }
                 var buttonBelow = event.target.parentElement.parentElement.lastElementChild;
                 if (y == 2) {
                     buttonBelow.style.display = "inline";
@@ -124,43 +99,42 @@ for(var x=0; x<allQuestions.length; x++){
                 }
             }, false)
         }
-    }}
+    }
+}
+
+
 
 
 var answerKey = ["Plano, Texas","University of California, San Diego","MalteseYorkie","Friends"];
 var numberCorrect = 0;
 
-for(var v=0; v<allQuestions.length; v++){
-    (function(correctAnswer){
+for (var v = 0; v < allQuestions.length; v++){
+    (function (correctAnswer) {
         var nextButton = document.getElementsByClassName("question")[v].lastElementChild;
-        nextButton.addEventListener("click",function(event){
+        nextButton.addEventListener("click", function (event) {
             var userAnswer;
-            for (var x = 0; x < event.target.previousSibling.previousSibling.getElementsByTagName("input").length; x++) {
-                if (event.target.previousSibling.previousSibling.getElementsByTagName("input")[x].checked) {
+            var numberOfChoices = event.target.previousSibling.previousSibling.getElementsByTagName("input");
+            for (var x = 0; x < numberOfChoices.length; x++) {
+                if (numberOfChoices[x].checked) {
                     if (userAnswer == undefined) {
-                        userAnswer = event.target.previousSibling.previousSibling.getElementsByTagName("input")[x].value
+                        userAnswer = numberOfChoices[x].value
                     } else {
-                        userAnswer += event.target.previousSibling.previousSibling.getElementsByTagName("input")[x].value
+                        userAnswer += numberOfChoices[x].value
                     }
-
-                    var userScore = function(){document.getElementById("welcomeScore").innerHTML = "Your score: " + numberCorrect};
+                    var userScore = function () {
+                        topBanner.innerHTML = "Your score: " + numberCorrect + "/" + allQuestions.length
+                    };
                     if (userAnswer === correctAnswer) {
                         numberCorrect++;
                         userScore();
                     } else {
                         userScore();
                     }
-
-
-
-
-
                 }
 
             }
             
         })
-        
     })(answerKey[v]);
 }
 
